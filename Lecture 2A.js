7b6e2989834b4169b8811401de155b7d7249e411
Source Academy User <No public email provided>
2024-08-21 16:40:06 +0800

    Changes made from Source Academy

--- Lecture 2A.js
+++ Lecture 2A.js
@@ -2,7 +2,8 @@
 evaluate operation 
 evaluate const 
 1.5.1 example for substitution model */
-/*const cost_per_meter = 199.5; 
+
+const cost_per_meter = 199.5; 
 function circumference(radius){
     return 2 * math_PI * radius;
 }
@@ -20,7 +21,7 @@ evaluate the argument first and apply function from top to down
 normal order reduction: do not evaluate the argument until nor more function 
 can be applied*/
 
-/*function sq ( x ) {
+function sq ( x ) {
     return x * x ;
  }
 function sum_of_sqs (x , y ) {
@@ -38,7 +39,7 @@ and then evalutate the statement(4)*/
 /*1.2.1 and 1.2.2 recursion and iteration 
 
 1.stackn and repeat_pattern */
-/*import {show, stack, heart, stack_frac, sail} from "rune";
+import {show, stack, heart, stack_frac, sail} from "rune";
 
 stack_frac (1 / 4 , heart ,stack_frac (1 / 3 , heart ,
 stack_frac (1 / 2 , heart , heart)));
@@ -60,7 +61,7 @@ itself*/
 square twice  return square(square(2)
 square 3 times: 
 square(square twice)*/
-/*function repeat_pattern(n, pat, init){
+function repeat_pattern(n, pat, init){
     return n === 0 ? init :pat(repeat_pattern(n - 1, pat, init));
 }
 
@@ -70,15 +71,15 @@ until no more to expand */
 
 //factorial function 
 
-//function factorial(n){
-   // return n === 0 ? 1/* basement line */: n * factorial(n - 1); }
-//factorial(10); 
+function factorial(n){
+   return n === 0 ? 1/* basement line */: n * factorial(n - 1); }
+factorial(10); 
 
 // write ilterative factorial 
-//function iter(product, counter, n){
-     //return counter > n ? product : iter(counter * product, 
-    // counter + 1, n);
-//} 
+function iter(product, counter, n){
+     return counter > n ? product : iter(counter * product, 
+     counter + 1, n);
+} 
 
 //iter(1, 1, 5); */
 
@@ -95,9 +96,9 @@ fib(11);
 
 
 
-
-
-
+// recursive: the function operation approaches the base case, 
+// while in iterative the controal variable approach the terminal value 
+// there is deferred operation accumulate in recursive 
 
 
 
