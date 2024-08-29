b4edf694bb8400212f6eec23b8ba1ab540b2c123
Source Academy User <No public email provided>
2024-08-29 11:42:50 +0800

    Changes made from Source Academy

--- Lecture 3A 2.0.js
+++ Lecture 3A 2.0.js
@@ -18,4 +18,74 @@ the rabbit choose jump, n-n=0, 1 way
 
 rick_the_rabbit(3);
 
+/*solve a question 
+eg coin change 
+1. read the question careful 
+different types of coins: 5, 10, 20, 50, 100 
+2. start with simple case, smaller question play with examples 
+10 (5,5) (10)
+20(5,5,5,5)(5,5,10)(10,10)(20)
+3. wishful thinking / existing solution 
+idea: highest coin is either 100 or not 100 
+in the first case; smaller problem 
+in the second case: smaller problem 
+base case: amount = 0 
+4. program with source */
+
+
+
+
+function first_ denomination (kinds_of_coins) {
+return kinds_of_coins === 1 ? 5 :
+kinds_of_coins === 2 ? 10 :
+kinds_of_coins === 3 ? 20 :
+kinds_of_coins === 4 ? 50 :
+kinds_of_coins === 5 ? 100 : 0;
+}
+
+// nested conditional expression 
+
+function cc(amount , kinds_of_coins) {
+return amount === 0
+? 1 
+: amount < 0 || kinds_of_coins = 0
+? 0
+/* base cases */
+: cc(amount -
+first_ denomination (kinds_of_coins) ,
+kinds_of_coins)
++
+cc (amount , kinds_of_coins - 1);
+}
+
+/*got two possiblity here, either we use that first coin( the 100 ), 
+substract from the total amount,and recall the coin change function 
+the other posibility is we do not use that coin, the amount is not changed 
+just straight away remove the largest coin 
+and when the function keep being recalled, 
+the amount decrease and the kindsof coin also decrease, it is just 
+either use the currenly highest denomination  or not 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
