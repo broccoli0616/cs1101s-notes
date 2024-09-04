f264b6ea7e85caa9abc58c5655d400ff6c2f77f0
Source Academy User <No public email provided>
2024-09-04 10:31:36 +0800

    Changes made from Source Academy

--- Lecture 4A.js
+++ Lecture 4A.js
@@ -1,10 +1,21 @@
-/rr
+/* data abstraction 
+1. types of values in source 
+- number 
+-boolean values 
+-strings
+-function 
+- some others 
+eg: fisrt-denomination function is the data structure 
+- function construct the data structure 
+- select component 
+
+combine different value in structure, provide way to access them 
 
 
-
-
-
-
+2.simplest data structure - pair 
+- constructed in math using tuple(set has order
+notation (0.5, 0.25)
+-selected in math using a pattern 
 
 
 
