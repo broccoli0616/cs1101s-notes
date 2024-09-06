b4b6cf66b5d5c15c468b2de17b79077750655e0d
Source Academy User <No public email provided>
2024-09-06 17:32:43 +0800

    Changes made from Source Academy

--- Lecture 4B.js
+++ Lecture 4B.js
@@ -1,5 +1,117 @@
+/* refinition of list discipline 
+- a list of a certain data type 
+is null or a pair whose head is of that data type and  whose tail 
+is a list of that data type */
+const listA = pair(1, pair(2, null));
+// list of number 
+const listD = NULL // a list of any data type 
 
-bnh
+// computing the length of a list 
+/* the length of the empty list is 0, and the length of a non-empty 
+list is one more than the length */
+
+function length(xs) {
+ return is_null(xs)
+ ? 0 
+ : 1 + length(tail(xs));
+}
+
+function length_iter(xs) {
+ function len(ys, counted_so_far) {
+ return is_null(ys)
+ ? counted_so_far
+ : len(tail(ys), counted_so_far + 1);
+ }
+ return len(xs, 0);
+}
+
+//appending two lists 
+function append2(xs, ys){
+    return is_null(xs)
+        ? is_nus
+        : ys
+        : pair(head(xs), append2(tail(xs), ys));
+}
+
+
+/*order of growth: how many function is called
+order of growth = length of xs
+recursive: the function pair will be deferred 
+until the inner function return a value
+length of xs = number of pair call 
+time is no. of function call
+space is the no. of deferred ops + new data structure 
+(some ppl also used the word variables) created */
+
+
+//reserve list 
+function reverse2(lst){
+    return is_null(lst)
+        ? null 
+        : append(reverse2(tail(lst)),
+        list(head(lst))); // pair(head(lst), null)
+}
+        
+/*call reverse n times but each time we call reserve we are calling 
+append as well, so run time is n^2
+although every recursive call to reverse2, 
+we are operating on a smaller list. */
+
+//list(head(lst)) = pair(head(lst), null)?
+
+// shorter running time, iterative process
+function reserve3(xs){
+    function rev(original, reversed){
+        return is_null(original)
+        ? reserved 
+        : rev(tail(original), pair(head(original), reserved));
+    }
+    return(xs, null);
+}
+
+/* abstraction map */
+function map(f, xs){
+    return is_null(xs)
+    ? null 
+    : pair(f(head(xs)), map(f, tail(xs)));
+}
+
+// use lambda expression 
+function scale_list(xs, k){
+    return map(x => k*x, xs);
+    
+}
+
+// taking even numbers from a list 
+function even_numbers(xs) {
+ return is_null(xs)
+ ? null
+ : head(xs) % 2 === 0
+ ? pair(head(xs), even_numbers(tail(xs)))
+ : even_numbers(tail(xs));
+}
+
+/* filter, to use predicate to choose if you want to keep the value or not 
+predicate can be also written as a value */
+function filter(pred, xs) {
+ return is_null(xs)
+ ? null
+ : pred(head(xs))
+ ? pair(head(xs), filter(pred, tail(xs)))
+ : filter(pred, tail(xs));
+}
+
+function even_numbers(xs) {
+ return filter(x => x % 2 === 0, xs);
+}
+
+/* accumulate */
+function accumulate(op, initial, xs) {
+ return is_null(xs)
+ ? initial
+ : op(head(xs), 
+ accumulate(op, initial, tail(xs)));
+}
 
 
 
@@ -10,6 +122,16 @@ bnh
 
 
 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+}
 
 
 
