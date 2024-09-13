ff667c603878441e5cf42c40b86b0f2f2310eac4
Source Academy User <No public email provided>
2024-09-13 11:22:49 +0800

    Changes made from Source Academy

--- Lecture 5B.js
+++ Lecture 5B.js
@@ -1,5 +1,38 @@
+1. javascipt is the target language 
+x 86-64 is the source language -- is the interpreter which is used to run game 
+written in javascript
+2. backend; x86-64 are both a source language and a hardware
+architecture and the machine language that the hardware understands
+The interpretor is written in x86 and understands elixir, so it 
+can translate elixir into x86 instructions to be executed on the processor 
+
+interpretor written in javascript can understand source languge which is used 
+to write the programme, can be directly run in the hardware (browser with java 
+script interpreter)
+
+2. compiler, translate one language to another 
+compiler hardware: Its the processor we use when we do the compilation.
+compilation process will happen at the browser with certain language 
+
+each compiler will have crorepsonding hardware, if it is in broswser interpreter
+need a real hardware interpreter to process 
+
+if you want to run on a x86-64 hardware , need have a x86-64 interpreter 
+higher order language to lower order langauge, use the compiler to quickly go to 
+the hardware language where hardware run the programme 
+
+An interpreter actually runs your programs. 
+A compiler just translates them. Different thing. Not comparable.
 
 
+a programme should be run in the hardware while a compilation process will 
+happen at the browser with certain language like javascript browser, 
+and with series of interpreter, the compilation process will be eventually 
+executed at the x86-64 hardware which is the computer
+
+the hardware interpreter run the programme eventually and 
+programme will be run at each Interpretation. Can I understand in 
+the way that compilation is a programme that need to be run at the hardware as well? 
 
 
 
