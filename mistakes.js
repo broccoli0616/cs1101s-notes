024435520c4a8920a12faefe9714fb3994386e05
Source Academy User <No public email provided>
2024-10-22 16:20:56 +0800

    Changes made from Source Academy

--- mistakes.js
+++ mistakes.js
@@ -28,35 +28,45 @@ let a = -1;
         }
     }
 }
-
-2) function my_first_filter(src, dest) {
-    const width = image_width();
-    const height = image_height();
-    const x = 1/2 * width;
-    const y = 1/2 * height;
-    for (let j = 0; j < height; j = j + 1) {
-        for (let i = 0; i < width; i = i + 1) {
-            if(absolute(j - y) < 1/4 * height && 
-               absolute(i - x) < 1/4 * width){
-                   let a = j;
-                   let b = i;
-                   if(j < y){
-                        a = j - 1/4 * height;
-                   } else { a = j + 1/4 * height;}
-                   if(i < x){
-                        b = i - 1/4 * width;
-                   } else { b = j + 1/4 * width;}
-            dest[a][b][0] = src[j][i][0]; // invert red intensity
-            dest[a][b][1] = src[j][i][1]; // invert green intensity
-            dest[a][b][2] = src[j][i][2];// invert blue intensity
-            dest[a][b][3] = 255;  // always 255
-               }
-          
-        }
-    }
+// debug
+2) function m_cc(amount, kinds_of_coins){
+    function first_denomination(kinds_of_coins) {
+          return kinds_of_coins === 1 ? 5 :
+                 kinds_of_coins === 2 ? 10 :
+                 kinds_of_coins === 3 ? 20 :
+                 kinds_of_coins === 4 ? 50 :
+                kinds_of_coins === 5 ? 100 : 0;
+    let mem = [];
+    let x = amount; 
+    let y = kinds_of_coins;
+    for(let i = x; i > 0; i = i - 1){
+     mem[x] = [];
+    for(let j = y; j > 0; j = j - 1){
+    if(mem[x][y] !== undefined){
+       return mem[x][y];
+        }else{
+              const result = x === 0
+                             ? 1
+                             : x < 0 || y === 0
+                             ? 0
+                             : m_cc(x, y - 1)
+                             +
+                              m_cc(x - first_denomination(y), y);
+                   write(x, y, result);
+               return result;
+      }
+        
+    function write(x,y,value) {
+    if (mem[x] === undefined) {
+        mem[x] = [];
+    }else { }
+     mem[x][y] = value;
+ }
 }
-
-install_filter(my_first_filter);
+}
+}
+}
+m_cc(100, 5);
 
 3) 
 function compose(filter1, filter2) {
