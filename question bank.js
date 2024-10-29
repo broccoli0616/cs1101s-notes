410c6514531e7482bfa70678500e655721274d4e
Source Academy User <No public email provided>
2024-10-29 12:06:43 +0800

    Changes made from Source Academy

--- question bank.js
+++ question bank.js
@@ -9,7 +9,7 @@ function list_of_instrument_sound(list_arpeggio){
     return consecutively(list_of_instrument_sound(list_arpeggio));
     
 2.(mission 9)
-/* qn: 
+ qn: 
 function simplify_rhythm(rhythm) {
     function repeatition(rhythm, n){
         return n < 1
@@ -25,5 +25,24 @@ function simplify_rhythm(rhythm) {
 
 why is_list first can run but is_pair cannot run  
 
+3) 1231 tut 
+function invest(n){
+    let result = 0;
+    for(let i = 0;i <= n; i = i + 1){
+        for(let j = 0; j <= n - i; j = j + 1){
+            for(let a = 0;a <= n - i - j ; a = a + 1){
+                 for(let b = 0;b <= n - i - j -a ; b = b + 1){
+                       let c = n - i - j - a - b;
+                    if(c >= 0){
+                     result = result + 1;
+                     
+                 }
+                 }
+                }
+            }
+        }
+  return result;
+}
 
+invest(50);
 
