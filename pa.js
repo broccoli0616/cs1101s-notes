13f3d2a9edfbfe7006289f57dea58186aabe73d2
Source Academy User <No public email provided>
2024-11-09 11:28:28 +0800

    Changes made from Source Academy

--- pa.js
+++ pa.js
@@ -718,7 +718,34 @@ function mul_stream(a, b) {
 }
 const B = pair(1, () => mul_stream(B, integers));
 
-// studio/ reflection function 
+function stream_append_pickle(xs, ys) {
+     return is_null(xs)
+            ? ys()
+            : pair(head(xs),
+                  () => stream_append_pickle(stream_tail(xs),
+
+                                              ys));
+
+}
+function integers(n){
+    return pair(n, () => integers(n + 1));
+}
+const int = integers(1);
+const int2 = pair(1, () => pair(2, () => pair(3, 
+                                       () => pair(4, () => null))));
+
+function stream_pair3(s){
+    function streamMaker(a, b){
+        return b === a? null : pair(pair(a, b), () => streamMaker(a + 1, b));
+    }
+    return stream_append_pickle(streamMaker(1, head(s)), () => stream_pair3(stream_tail(s)));
+}
+
+const test = map_stream(int, stream_tail(int));
+eval_stream(test, 5);
+
+
+
 // debug notes 
 // predeclared 
 // online links 
